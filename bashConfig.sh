8458b1f26c27365213f17dcd2a80c798e7678aa7
savage <>
2020-04-06 20:02:09 -0400

    update umatrix

--- bashConfig.sh
+++ bashConfig.sh
@@ -1,7 +1,7 @@
+source H:/Desktop/Git/config/bashConfig_colors.sh
 source H:/Desktop/Git/config/bashConfig_export.sh
 source H:/Desktop/Git/config/bashConfig_shortcuts.sh
 source H:/Desktop/Git/config/bashConfig_git.sh
 source H:/Desktop/Git/config/bashConfig_programming.sh
-source H:/Desktop/Git/config/bashConfig_colors.sh
 source H:/Desktop/Git/config/bashConfig_personal.sh
 
