8a87b58b698630f9b6eb24160c368b3ce560ab8c
savage <>
2020-07-09 20:09:01 -0400

    stuff

--- bashConfig_exports.sh
+++ bashConfig_exports.sh
@@ -1,10 +1,4 @@
-new_ps1="\n${Red}=====[${Color_Off}\
-${Cyan}\T${Color_Off} \
-${Yellow}\w${Color_Off}\
-${Red}]=====${Color_Off}\n\$ "
-
 export EDITOR='npp -w'
-export PS1="${new_ps1}"
 export HISTTIMEFORMAT='%b %d %I:%M %p ' # using strftimr format
 export HISTCONTROL=ignoreboth # ignoredups:ignorespace
 export HISTIGNORE="history:pwd:exit:df:ls:ls -la:ll:l:c:x:d:h:bc:cd:src:"
